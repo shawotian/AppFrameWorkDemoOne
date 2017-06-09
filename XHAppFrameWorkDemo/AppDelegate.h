9f3cd51208601c1c73f53de7b302279b5a89c4ae
YLY <tianxiaoxiao@bldz.com>
2017-06-09 16:58:16 +0800

    Initial Commit

---
+++ XHAppFrameWorkDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XHAppFrameWorkDemo
+//
+//  Created by YLY on 2017/6/9.
+//  Copyright © 2017年 YLY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
